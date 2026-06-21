b7cf97c56f018d68cbc3c37fdf300fb1548028de
kotchamiogougbe-a11y <kotchamiogougbe@gmail.com>
2026-06-21 05:38:45 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Soli Deo Gloria
 ═══════════════════════════════════════════════════════════ */
 
-var CACHE = "kotchami-v1";
+var CACHE = "kotchami-v3";
 
 // Ressources de base mises en cache dès l'installation
 var CORE = [
